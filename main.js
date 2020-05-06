48bd343532a05226db84cb39f812b52b1d301635
pietro <pietro1990@hotmail.it>
2020-05-06 18:21:32 +0200

    aggiunto il footer

--- main.js
+++ main.js
@@ -1,9 +1,8 @@
 $(document).ready(function() {
 
     //intercetto il click su una voce principale di menu;
-    $('.s').click(function() {
+    $('.icona').click(function() {
         //recupero il tag ul con classe dropdown che è suo fratello
-
         var sottomenu = $(this).next('.dropdown');
         //valuto se l'elemento su cui ho cliccato ha gia il dropdawn aperto;
         if(sottomenu.is(':visible')){
@@ -19,7 +18,16 @@ $(document).ready(function() {
     })
 
     $('.a').click(function(){
-        $('.dropdown').toggle();
+        var sottomenu = $('div.dropdown')
+        if(sottomenu.is(':visible')){
+            //se si allora ho cliccato lastessa voce per chiuderlo;
+            sottomenu.hide();
+        } else {
+            //se no allora ho cliccato una nuova voce per aprirlo;
+            //chiudo gli altri eventuali aperti in precedenza;
+            $('.dropdown').hide();
+            //visuallizzo il menu dropdown
+            sottomenu.show();
+        }
     })
-
 })
