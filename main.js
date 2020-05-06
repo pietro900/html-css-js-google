4d8442a405eae6bd5fa717c744f825cc6f52c2d4
pietro <pietro1990@hotmail.it>
2020-05-06 16:44:39 +0200

    esercizio quasi completo

---
+++ main.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+
+    //intercetto il click su una voce principale di menu;
+    $('.s').click(function() {
+        //recupero il tag ul con classe dropdown che è suo fratello
+
+        var sottomenu = $(this).next('.dropdown');
+        //valuto se l'elemento su cui ho cliccato ha gia il dropdawn aperto;
+        if(sottomenu.is(':visible')){
+            //se si allora ho cliccato lastessa voce per chiuderlo;
+            sottomenu.hide();
+        } else {
+            //se no allora ho cliccato una nuova voce per aprirlo;
+            //chiudo gli altri eventuali aperti in precedenza;
+            $('.dropdown').hide();
+            //visuallizzo il menu dropdown
+            sottomenu.show();
+        }
+    })
+
+    $('.a').click(function(){
+        $('.dropdown').toggle();
+    })
+
+})
